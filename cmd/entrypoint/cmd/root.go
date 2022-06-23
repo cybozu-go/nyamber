1ee4b8e4a5ac6030695fc190e8354977b5b35f2a
terashima <tomoya-terashima@cybozu.co.jp>
2022-06-23 12:04:33 +0900

    add status endpoint

--- cmd/entrypoint/cmd/root.go
+++ cmd/entrypoint/cmd/root.go
@@ -6,15 +6,32 @@ import (
 
 	"github.com/cybozu-go/nyamber/pkg/constants"
 	"github.com/cybozu-go/well"
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 var listenAddr string
+var log logr.Logger
+
+type StatusResponse struct {
+	Job []JobStatus `json:"jobs"`
+}
+
+type JobStatus struct {
+	Name      string `json:"name"`
+	Status    string `json:"status"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
+}
 
 var rootCmd = &cobra.Command{
-	Use:   "entrypoint",
-	Short: "DC test pod entrypoint",
-	Long:  "DC test pod entrypoint",
+	Use:          "entrypoint <JOB_NAME:COMMAND_PATH>...",
+	Short:        "DC test pod entrypoint",
+	Long:         "DC test pod entrypoint",
+	Args:         cobra.MinimumNArgs(1),
+	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// run neco bootstrap
 		// run neco-apps bootstrap
@@ -23,17 +40,17 @@ var rootCmd = &cobra.Command{
 		mux.Handle("/"+constants.StatusEndPoint, http.HandlerFunc(statusHandler))
 		serv := &well.HTTPServer{
 			Server: &http.Server{
-				Addr:    constants.ListenPort,
+				Addr:    listenAddr,
 				Handler: mux,
 			},
 		}
+		log.Info("Entrypoint server start")
 		if err := serv.ListenAndServe(); err != nil {
 			return err
 		}
 
-		env.Stop()
-		return env.Wait()
-		return nil
+		well.Stop()
+		return well.Wait()
 	},
 }
 
@@ -52,4 +69,9 @@ func Execute() error {
 func init() {
 	fs := rootCmd.Flags()
 	fs.StringVar(&listenAddr, "listen-address", fmt.Sprintf(":%d", constants.ListenPort), "Listening address and port.")
+	zapLog, err := zap.NewDevelopment()
+	if err != nil {
+		panic(fmt.Sprintf("who watches the watchmen (%v)?", err))
+	}
+	log = zapr.NewLogger(zapLog)
 }
