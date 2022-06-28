f2a3e2faf161c61acea943e8609176fbb6b5a45e
Yusuke ARAI <yusuke.arailly@gmail.com>
2022-06-28 11:00:34 +0900

    wip envtest
    Signed-off-by: Yusuke ARAI <yusuke.arailly@gmail.com>

---
+++ controllers/virtualdc_controller_test.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("VirtualDC controller", func() {
+	It("should sync an application", func() {
+		Expect(true).To(BeTrue())
+	})
+})
