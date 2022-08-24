2f59d5c1cb6ff64335cac974518d3a95c54e301e
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-08-24 07:53:07 +0000

    create VirtualDC by AutoVirtualDC

--- controllers/autovirtualdc_controller.go
+++ controllers/autovirtualdc_controller.go
@@ -19,12 +19,15 @@ package controllers
 import (
 	"context"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	nyamberv1beta1 "github.com/cybozu-go/nyamber/api/v1beta1"
+	"github.com/cybozu-go/nyamber/pkg/constants"
 )
 
 // AutoVirtualDCReconciler reconciles a AutoVirtualDC object
@@ -47,10 +50,35 @@ type AutoVirtualDCReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.2/pkg/reconcile
 func (r *AutoVirtualDCReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	avdc := &nyamberv1beta1.AutoVirtualDC{}
+	if err := r.Get(ctx, req.NamespacedName, avdc); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
+	if !avdc.ObjectMeta.DeletionTimestamp.IsZero() {
+		finalizeResult, err := r.finalize(ctx, avdc)
+		if err != nil {
+			logger.Error(err, "Finalize error")
+			return ctrl.Result{}, err
+		}
+		return finalizeResult, nil
+	}
+
+	if !controllerutil.ContainsFinalizer(avdc, constants.FinalizerName) {
+		controllerutil.AddFinalizer(avdc, constants.FinalizerName)
+		err := r.Update(ctx, avdc)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	if err := r.createVirtualDC(ctx, avdc); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	logger.Info("reconcile succeeded")
 	return ctrl.Result{}, nil
 }
 
@@ -59,4 +87,26 @@ func (r *AutoVirtualDCReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&nyamberv1beta1.AutoVirtualDC{}).
 		Complete(r)
+}
+
+func (r *AutoVirtualDCReconciler) createVirtualDC(ctx context.Context, avdc *nyamberv1beta1.AutoVirtualDC) error {
+	logger := log.FromContext(ctx)
+	vdc := &nyamberv1beta1.VirtualDC{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      avdc.Name,
+			Namespace: avdc.Namespace,
+		},
+		Spec: avdc.Spec.Template.Spec,
+	}
+	err := r.Create(ctx, vdc)
+	if err != nil {
+		return err
+	}
+	logger.Info("VirtualDC created")
+
+	return nil
+}
+
+func (r *AutoVirtualDCReconciler) finalize(ctx context.Context, avdc *nyamberv1beta1.AutoVirtualDC) (ctrl.Result, error) {
+	return ctrl.Result{}, nil
 }
