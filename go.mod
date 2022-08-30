e32a800fd10ed2b80490be7c3e037aff0f7e7927
speed1313 <speedtry13@icloud.com>
2022-08-30 09:31:20 +0000

    implement webhook

--- go.mod
+++ go.mod
@@ -76,6 +76,7 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/robfig/cron v1.2.0
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5 // indirect
--- hooks/autovirtualdc.go
+++ hooks/autovirtualdc.go
@@ -18,9 +18,14 @@ package hooks
 
 import (
 	"context"
+	"time"
 
 	nyamberv1beta1 "github.com/cybozu-go/nyamber/api/v1beta1"
+	"github.com/robfig/cron"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -43,8 +48,15 @@ type autoVirtualdcValidator struct{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (v autoVirtualdcValidator) ValidateCreate(ctx context.Context, obj runtime.Object) error {
 	logger := log.FromContext(ctx)
-	logger.Info("validate create", "name", obj.(*nyamberv1beta1.AutoVirtualDC).Name)
+	avdcName := obj.(*nyamberv1beta1.AutoVirtualDC).Name
+	logger.Info("validate create", "name", avdcName)
 
+	errs := v.validateCommon(obj)
+	if len(errs) > 0 {
+		err := apierrors.NewInvalid(schema.GroupKind{Group: nyamberv1beta1.GroupVersion.Group, Kind: "AutoVirtualDC"}, avdcName, errs)
+		logger.Error(err, "validation error", "name", avdcName)
+		return err
+	}
 
 	return nil
 }
@@ -52,9 +64,15 @@ func (v autoVirtualdcValidator) ValidateCreate(ctx context.Context, obj runtime.
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (v autoVirtualdcValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) error {
 	logger := log.FromContext(ctx)
-	avdcName := oldObj.(*nyamberv1beta1.AutoVirtualDC).Name
+	avdcName := newObj.(*nyamberv1beta1.AutoVirtualDC).Name
 	logger.Info("validate update", "name", avdcName)
 
+	errs := v.validateCommon(newObj)
+	if len(errs) > 0 {
+		err := apierrors.NewInvalid(schema.GroupKind{Group: nyamberv1beta1.GroupVersion.Group, Kind: "AutoVirtualDC"}, avdcName, errs)
+		logger.Error(err, "validation error", "name", avdcName)
+		return err
+	}
 
 	return nil
 }
@@ -62,4 +80,34 @@ func (v autoVirtualdcValidator) ValidateUpdate(ctx context.Context, oldObj, newO
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (v autoVirtualdcValidator) ValidateDelete(ctx context.Context, obj runtime.Object) error {
 	return nil
+}
+
+func (v autoVirtualdcValidator) validateCommon(obj runtime.Object) field.ErrorList {
+	var errs field.ErrorList
+	avdc :=  obj.(*nyamberv1beta1.AutoVirtualDC)
+
+	if (avdc.Spec.StartSchedule != "" && avdc.Spec.StopSchedule == "") || (avdc.Spec.StartSchedule == "" && avdc.Spec.StopSchedule != ""){
+		errs = append(errs, field.Forbidden(field.NewPath("spec", "startSchedule"), "specifing only one side is not allowed"))
+		errs = append(errs, field.Forbidden(field.NewPath("spec", "stopSchedule"), "specifing only one side is not allowed"))
+	}
+
+	if avdc.Spec.StartSchedule != "" && avdc.Spec.StopSchedule != ""{
+		specParser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+		_, err := specParser.Parse(avdc.Spec.StartSchedule)
+		if err != nil {
+			errs = append(errs, field.Invalid(field.NewPath("spec", "startSchedule"), avdc.Spec.StartSchedule, "the field can not be parsed"))
+		}
+		_, err = specParser.Parse(avdc.Spec.StopSchedule)
+		if err != nil {
+			errs = append(errs, field.Invalid(field.NewPath("spec", "stopSchedule"), avdc.Spec.StopSchedule, "the field can not be parsed"))
+		}
+	}
+
+	_, err := time.ParseDuration(avdc.Spec.TimeoutDuration)
+	if err != nil {
+		errs = append(errs, field.Invalid(field.NewPath("spec", "timeoutDuration"), avdc.Spec.TimeoutDuration, "the field can not be parsed"))
+	}
+
+	return errs
+
 }
