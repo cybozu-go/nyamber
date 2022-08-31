f362166bd0be78fd6fd0987ad7cb81a0bbf0180b
speed1313 <speedtry13@icloud.com>
2022-08-31 16:54:01 +0900

    add avdc e2e test

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -16,7 +16,7 @@ import (
 
 var _ = Describe("Nyamber", func() {
 	It("should prepare", func() {
-		vdcs := []string{"vdc_testcase", "vdc_testcase2", "vdc_testcase3"}
+		vdcs := []string{"vdc_testcase", "vdc_testcase2", "vdc_testcase3", "avdc_testcase"}
 		_, err := kubectl(nil, "apply", "-f", "./manifests/namespace.yaml")
 		Expect(err).Should(Succeed())
 		for _, v := range vdcs {
@@ -24,6 +24,11 @@ var _ = Describe("Nyamber", func() {
 			_, err := kubectl(nil, "apply", "-f", fmt.Sprintf("./manifests/%s.yaml", v))
 			Expect(err).Should(Succeed())
 		}
+	})
+
+	It("should deny invalid avdc manifest", func() {
+		_, err := kubectl(nil, "apply", "-f", "./manifests/avdc_testcase2.yaml")
+		Expect(err).ShouldNot(Succeed())
 	})
 
 	It("should create resources", func() {
@@ -112,6 +117,32 @@ var _ = Describe("Nyamber", func() {
 			}).Should(Succeed())
 		}
 	})
+
+	It("should create vdc according to avdc", func() {
+		By("checking vdc is created")
+		Eventually(func() (*nyamberv1beta1.VirtualDC, error) {
+			out, err := kubectl(nil, "get", "virtualdc", "-n", "nyamber-pod", "auto-virtual-dc", "-o", "json")
+			if err != nil {
+				return nil, err
+			}
+			vdc := &nyamberv1beta1.VirtualDC{}
+			err = json.Unmarshal(out, vdc)
+			if err != nil {
+				return nil, err
+			}
+			return vdc, nil
+		}, 5).Should(
+			PointTo(MatchFields(IgnoreExtras, Fields{
+				"Spec": MatchFields(IgnoreExtras, Fields{
+					"NecoBranch":     Equal("release"),
+					"NecoAppsBranch": Equal("release"),
+					"Command":        Equal([]string{"env"}),
+				}),
+			})),
+		)
+	})
+
+
 
 	It("should execute all commands correctly", func() {
 		testcases := []struct {
